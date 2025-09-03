b5c11fcbfb2425b8f7ab1c14c12c22623195105a
efahnjoe <efahnjoe@gmail.com>
2025-09-04 00:19:13 +0800

    feat(router): set up `Vue Router` and path aliases
    - Integrate unplugin-vue-router and configure in Vite config
    - Enable path aliases for cleaner imports
    - Initialize router in `src/renderer/src/router.ts`

---
+++ src/renderer/src/router.ts
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { routes } from "vue-router/auto-routes";
+
+export default createRouter({
+  history: createWebHistory(),
+  // pass the generated routes written by the plugin
+  routes
+});
